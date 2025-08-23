49674328d0634d328838938205d55730daefd256
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-23 21:03:36 +0600

    Update: New Changes

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 document.getElementById("loginBtn").addEventListener("click", function(event){
     event.preventDefault();
     const mobileNumber = '01716884927'; 
-    const pinNumber = 3456;
+    const pinNumber = 2468;
     const userinpMobile = document.getElementById("mobileNum").value; // The Mobile Number Stored in String So, I did not casted the type of the Mobile Number
     const userinpPin = document.getElementById("pinNum").value;
     const Pin = parseInt(userinpPin);
