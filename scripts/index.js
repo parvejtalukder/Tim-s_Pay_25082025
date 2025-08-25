45bca99bc64faaeb62c1df81f7dd28f4aa90f555
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-25 15:25:22 +0600

    Update: New File Resable

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,28 @@
-// console.log('This is a console');
-document.getElementById("loginBtn").addEventListener("click", function(event){
-    event.preventDefault();
-    const mobileNumber = '01716884927'; 
-    const pinNumber = 2468;
-    const userinpMobile = document.getElementById("mobileNum").value; // The Mobile Number Stored in String So, I did not casted the type of the Mobile Number
-    const userinpPin = document.getElementById("pinNum").value;
-    const Pin = parseInt(userinpPin);
-    // console.log(userinpMobile, Pin);
-    if (parseInt(mobileNumber) === parseInt(userinpMobile) && Pin === pinNumber) {
-        console.log("Mobile and Pin Number Are Ok");
-        window.location.href="./pages/home.html";    
-    } else {
-        console.log("Wrong Mobile or Pin");
-        alert("Wrong Mobile and Pin");
-    }
-});
+// FUNCTIONS
+function getVal(id) {
+    const value = document.getElementById(id).value;
+    return value;
+}
+function ifError() {
+    console.log("Wrong Information!");
+    alert("Wrong Information!");
+}
+function logIn (id) {
+    document.getElementById(id).addEventListener("click", function(event){
+        // Compare string from input field to string constant
+        if (getVal(mobileNum) === mobileNumber && parseInt(getVal(pinNum)) === pinNumber) {
+            console.log("All are Ok!");
+            window.location.href = "./pages/home.html";
+        } else {
+            ifError();
+        }
+    })
+}
+
+// FUNCTION CALLING
+const mobileNumber = '01716884927'; 
+const pinNumber = 2468;
+const loginBtn = 'loginBtn'; 
+const mobileNum = 'mobileNum';
+const pinNum = 'pinNum';
+logIn(loginBtn);
